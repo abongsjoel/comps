0d5e4aef07c1d3799e546b757754e9e45db4298d
Joel Chi <abongsjoel@gmail.com>
2024-11-25 14:46:59 +0100

    Add Accordion funtionality

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 function Accordion({ items }) {
-  const rendereditems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const rendereditems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
