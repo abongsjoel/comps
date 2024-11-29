4b99f4b274611e6169e9454924f059c31a6d0317
Joel Chi <abongsjoel@gmail.com>
2024-11-29 11:01:29 +0100

    Another variation of event Handlers

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,16 +3,16 @@ import { useState } from "react";
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
+
   const rendereditems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
-    const handleClick = () => {
-      setExpandedIndex(index);
-    };
-
     return (
       <div key={item.id}>
-        <div onClick={handleClick}>{item.label}</div>
+        <div onClick={() => handleClick(index)}>{item.label}</div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
