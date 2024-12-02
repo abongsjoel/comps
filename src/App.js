24034bd8051ce952df55e68c2d8d8a379a2670ff
Joel Chi <abongsjoel@gmail.com>
2024-12-02 19:51:14 +0100

    Create AccordionPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import React from "react";
-import Accordion from "./components/Accordion";
 
 function App() {
-  const items = [
-    {
-      id: "l1",
-      label: "Can I use React on a project?",
-      content: "You can use React on any project you want",
-    },
-    {
-      id: "l2",
-      label: "Can I use JavaScript on a project?",
-      content: "You can use JavaScript on any project you want",
-    },
-    {
-      id: "l3",
-      label: "Can I use TypeScript on a project?",
-      content: "You can use TypeScript on any project you want",
-    },
-  ];
-  return <Accordion items={items} />;
+  return <div>App</div>;
 }
 
 export default App;
