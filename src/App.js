18782bd009567c54cd8174514098e344d758ba2b
Joel Chi <abongsjoel@gmail.com>
2024-12-03 11:16:55 +0100

    Implement dropdown functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import Dropdown from "./components/Dropdown";
 
 function App() {
-  return <Dropdown />;
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
+  ];
+
+  return <Dropdown options={options} />;
 }
 
 export default App;
