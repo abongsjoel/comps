4110c595d44b580a780a4500451233724d2fb14e
Joel Chi <abongsjoel@gmail.com>
2024-12-04 17:52:08 +0100

    Implement controlled component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,19 +1,20 @@
 import { useState } from "react";
 
-function Dropdown({ options }) {
+function Dropdown({ options, selection, onSelect }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleOptionClick = () => {
+  const handleOptionClick = (option) => {
+    onSelect(option);
     setIsOpen(false);
   };
 
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={handleOptionClick} key={option.value}>
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
         {option.label}
       </div>
     );
@@ -21,7 +22,9 @@ function Dropdown({ options }) {
 
   return (
     <div>
-      <div onClick={handleClick}>Select...</div>
+      <div onClick={handleClick}>
+        {selection ? selection.label : "Select..."}
+      </div>
       {isOpen && <div>{renderedOptions}</div>}
     </div>
   );
