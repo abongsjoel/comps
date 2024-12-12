58ddbdfbf5ddd4a42331b95474b23721af23b3cc
Joel Chi <abongsjoel@gmail.com>
2024-12-12 01:21:42 +0100

    Create Route component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 // import React, { useState } from "react";
 
 import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
 
 function App() {
   return (
     <div>
       <Link to="accordion">Go to accordion</Link>
       <Link to="dropdown">Go to dropdownm</Link>
+      <Route path="/accordion">
+        <AccordionPage />
+      </Route>
+      <Route path="/dropdown">
+        <DropdownPage />
+      </Route>
     </div>
   );
 }
