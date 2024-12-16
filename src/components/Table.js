e6bb918d3bf74ca09f119f91da5914502817ebf1
Joel Chi <abongsjoel@gmail.com>
2024-12-16 23:42:46 +0100

    Add table content

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,25 @@
-function Table() {
-  return <div>Table</div>;
+function Table({ data }) {
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr>
+        <td>{fruit.name}</td>
+        <td>{fruit.color}</td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
+  });
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
 export default Table;
