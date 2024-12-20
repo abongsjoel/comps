a3658f7e97d47e5068d101bc5605f70e9fd38b83
Joel Chi <abongsjoel@gmail.com>
2024-12-20 22:02:29 +0100

    Update page counter component

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,15 +1,35 @@
 import React from "react";
 import Button from "../components/Button";
-import useCounter from "../hooks/use-counter";
+import Panel from "../components/Panel";
 
 const CounterPage = ({ initialCount }) => {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = React.useState(initialCount);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
 
   return (
-    <div>
-      <h1>Counter: {count}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Counter: {count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form>
+        <label>Add a lot!</label>
+        <input
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button type="submit">Add it</Button>
+      </form>
+    </Panel>
   );
 };
 
